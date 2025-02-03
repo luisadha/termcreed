7352cc45d27262809853945e448f960a1a04040c
luisadha <adharudin14@gmail.com>
2025-02-03 11:40:34 +0700

    Update to v0.02

--- termcreed.sh
+++ termcreed.sh
@@ -1,13 +1,29 @@
-
+#!/usr/bin/env bash
 # termcreed.sh
-# version: v0.01
-# Copyright (c) 2025 - Luis Adha
+# version: v0.02
+# Copyright (c) 2025 - Luis Adha (Don't rewrite these comments)
 
 
-# Your credential
+# Setting your credential informations
 username_valid="$(cat ~/.ssh/userid)";
 password_valid="$(age -d -i ~/.ssh/keyid.txt ~/.ssh/passid)"
 
+# Settings your text Interactive
+declare -u text_username="Nama Pengguna :"
+declare -u text_password="Kata Sandi :"
+declare if_sucstr="Login successful";
+declare if_failstr="Login failed";
+
+: "Main code"
+# Varibale sets
+readonly x=2
+readonly y=7
+readonly lineno_1=1
+readonly lineno_2=4
+ulenght=${#text_username}
+plenght=${#text_password}
+ucursor=$(( ulenght + 2 ))
+pcursor=$(( plenght + 2 ))
 sym="┃";
 border_split=('┏' '┓' '━' '┗' '┛')
 width=30
@@ -24,11 +40,8 @@ center_text() {
     local cols=$(tput cols)
     local text_length=${#text}
     local padding=$(( (cols - text_length) / 2 ))
-
     (( padding < 0 )) && padding=0
-
-    # Cetak teks dengan spasi di depan lalu pipe ke `boxes`
-    printf "%*s%s\n" "$padding" "" "$text" | boxes -d "$@" 
+    printf "%*s%s\n" "$padding" "" "$text" | boxes -d "$@"
 
 }
 drawTextFill() {
@@ -45,31 +58,29 @@ box() {
 align_top() {
 	printf %"$(echo "$(tput cols) - 2" | bc)"s | sed "s/ /${border_split[2]}/g" | sed "s/^/${border_split[0]}/g" | sed "s/$/${border_split[1]}/g"
 }
-
 align_bottom() {
 	printf %"$(echo "$(tput cols) - 2" | bc)"s | sed "s/ /${border_split[2]}/g" | sed "s/^/${border_split[3]}/g" | sed "s/$/${border_split[4]}/g"
 }
-
 sign_form() {
 align_top
-drawTextFill "${sym} ${1:-}" | sed "s/$/${sym}/g"
+drawTextFill "${sym} ${@}" | sed "s/$/${sym}/g"
 align_bottom
 }
 pasw_form() { sign_form "$@"; }
 
-# return
+#exit || return # for debug
 while :; do clear;
 
-
-tput sc; sign_form "SIGN IN:"; tput cup 1 11;  read username; tput cup 2 11; echo
-tput sc; pasw_form "PASSWORD:"; tput cup 4 11; read -s password; tput cup 5; echo
+tput sc; sign_form "$text_username"; tput cup $lineno_1 $ucursor; read username; tput cup $x; echo
+tput sc; pasw_form "$text_password"; tput cup $lineno_2 $pcursor; read -s password; tput cup $y;
 
  if [[ "$username" == "$username_valid" && "$password" == "$password_valid" ]]; then
-	 center_text "Login successful!" -d stone
+	    center_text "${if_sucstr}!" -d stone
     break
  else
-         center_text "Login failed!" -d f90-box
+         center_text "${if_failstr}!" -d f90-box
       sleep 1
     continue
  fi
 done
+
